e09536649d00130851a220200a6212d95dae0f1a
sunshay <sunshayina@outlook.com>
2022-05-16 13:30:29 -0700

    add server file

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from crypt import methods
+from unicodedata import name
 from flask import Flask, render_template, request #1.import flask module
+from flask_mysqldb import MySQL
 
 #2.instantiate an object for this class flask
 app = Flask(__name__)   #we pass in the current module identify by __name__
@@ -6,8 +9,12 @@ app = Flask(__name__)   #we pass in the current module identify by __name__
 #4.with the decorator route called on the app variable
 #forward a load on the server : repesented by (/)
 #The request is made on the (/)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST']) #10.add method GET and POST and add if request in the function to choose between POST or GET
 def index():
+    if request.method == 'POST': #11.store user info name and email 
+        userDetails = request.form 
+        name = userDetails['name']
+        email = userDetails['email']
      #6.return '<h1>Text example</h1>' or look for index page on the project
     return render_template('form.html')          
 
